68ba53226fe7f889a8f90e1bf00649c3e1cc91bf
ivan7enov <ivan7enov91@gmail.com>
2022-12-09 11:49:06 +0300

    recursion mirroring algorithm for binary trees

---
+++ BinaryTreeInversion/BTreeMirrorStackStrategy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BinaryTreeMirroring
+{
+    /// <summary>
+    /// Используется <see cref="Stack{T}"/> для хранения промежуточных результатов.
+    /// Плюс такой реализации в том, что, в отличие от рекурсивного алгоритма (<see cref="BTreeMirrorRecursionStrategy"/>), поддерживаются большие деревья.
+    /// Минус - при небольших размерах дерева менее эффективен, чем рекурсия. Для организации стека организуется Generic тип, который так же требует дополнительной памяти и обработки (увеличение размера, копирование массива и т.п.). Соответственно для работы с узлами дерева появляется дополнительный слой методов (pop/push), вызов которых чуть но отнимает ресурсов.
+    /// </summary>
+    public class BTreeMirrorStackStrategy : IBTreeMirrorStrategy
+    {
+        public void Mirror(Node root)
+        {
+            Stack<Node> stack = new Stack<Node>(new[] { root });
+
+            Node nodeToMirror;
+            do
+            {
+                nodeToMirror = stack.Pop();
+                if ((nodeToMirror.Left ?? nodeToMirror.Right) == null)
+                    continue;
+
+                (nodeToMirror.Left, nodeToMirror.Right) = (nodeToMirror.Right, nodeToMirror.Left);
+
+                if (nodeToMirror.Left != null)
+                    stack.Push(nodeToMirror.Left);
+                if (nodeToMirror.Right != null)
+                    stack.Push(nodeToMirror.Right);
+            }
+            while (stack.Any());
+        }
+    }
+}
